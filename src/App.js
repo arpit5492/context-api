88a1d79597defcfc0d9516940d298ce3e70fabfc
arpit5492 <arpitmallick99@gmail.com>
2024-04-01 23:00:58 -0700

    Added state for purchase component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Cart from './components/Cart';
 import Total from './components/Total';
 
 function App() {
+  const [prods, setProds] = useState([
+    {pName: "Apples", price: 2.34},
+    {pName: "Bananas", price: 3.21},
+    {pName: "Grapes", price: 6.78},
+    {pName: "Oranges", price: 5.43}
+  ]);
+
+  const prodItems = prods.map((prod, index) => {
+    return (
+      <option key={index}>{prod.pName} - {prod.price}</option>
+    )
+  });
   return (
     <div className="App">
       <div className="row pt-4">
@@ -12,10 +25,7 @@ function App() {
               <h4 className="card-title">Purchase Component</h4>
               <hr />
               <select className="form-select" aria-label="default select example">
-                <option>Apples - $2.34</option>
-                <option>Oranges - $3.21</option>
-                <option>Bananas - $5.43</option>
-                <option>Grapes - $6.78</option>
+                {prodItems}
               </select>
             </div>
           </div>
