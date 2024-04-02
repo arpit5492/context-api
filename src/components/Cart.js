f4c5143112b09c0c0a21a2449f589b38e20b0701
arpit5492 <arpitmallick99@gmail.com>
2024-04-01 20:40:21 -0700

    Component implemented

---
+++ src/components/Cart.js
@@ -0,0 +1,18 @@
+const Cart = () => {
+    return (
+        <div className="col-sm-4 mr-3">
+          <div className="card" style={{height: "300px"}}>
+            <div className="card-body">
+              <h4 className="card-title">Cart Component</h4>
+              <hr />
+              <p>Apples - $2.34</p>
+              <p>Oranges - $3.21</p>
+              <p>Bananas - $5.43</p>
+              <p>Grapes - $6.78</p>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Cart;
